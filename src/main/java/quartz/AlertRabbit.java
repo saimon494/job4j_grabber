410e40c9a33faae143977831e73f49914b0bc615
saimon494 <saimon.494@gmail.com>
2021-02-14 13:33:10 +0300

    1. Quartz [#408770]

---
+++ src/main/java/quartz/AlertRabbit.java
@@ -0,0 +1,45 @@
+package quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        try (InputStream in = AlertRabbit.class.
+                getClassLoader().
+                getResourceAsStream("rabbit.properties")
+        ){
+            config.load(in);
+            int interval = Integer.parseInt(config.getProperty("rabbit.interval"));
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(interval)
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
